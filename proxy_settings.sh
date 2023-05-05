060ecb4f3f6890c8516bafcb38f476feb0930fe9
Horacio Dalla Valle <dallavalle.h@pm.me>
2023-05-05 09:22:37 -0300

    Add some comments to my proxy settings script

--- proxy_settings.sh
+++ proxy_settings.sh
@@ -1,3 +1,8 @@
+# Put in /etc/profile.d
+#
+# https://wiki.archlinux.org/title/Proxy_server#Environment_variables
+# https://help.ubuntu.com/community/AptGet/Howto
+
 export http_proxy="http://internet:8080/"
 export https_proxy="http://internet:8080/"
 export no_proxy="localhost,127.0.0.1"
