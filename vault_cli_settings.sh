2ead73cde4f7856fd2ef244d4cf54116ff864993
Horacio Dalla Valle <dallavalle.h@pm.me>
2023-06-02 17:00:37 -0300

    Add a Vault CLI configuration script

---
+++ vault_cli_settings.sh
@@ -0,0 +1,6 @@
+# Vault environment variables: https://developer.hashicorp.com/vault/docs/commands#environment-variables
+
+export VAULT_ADDR=""  # Complete with Vault URL
+export VAULT_CACERT=""  # If applicable, complete with the path where is the cert of the trusted CA (typical in internal PKI environments)
+export VAULT_TOKEN=""  # If applicable, complete with the respective token
+
